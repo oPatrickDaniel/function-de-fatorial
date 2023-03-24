a2e1767bbd832d0cfd897f5f00b7f4db8796707f
Patrick Daniel <contato.pdani@gmail.com>
2023-03-24 20:41:09 -0300

    correção

--- stript.js
+++ stript.js
@@ -1,16 +1,15 @@
 function fatorial(num) {
-    var result = num;
+    let result = num;
     if (num === 0 || num === 1) {
-        console.log(1)
+
         return 1;
     } else {
         while (num > 1) {
+
             num--;
             result *= num;
         }
-        console.log(result)
+
         return result;
     }
 }
-
-fatorial(3)
