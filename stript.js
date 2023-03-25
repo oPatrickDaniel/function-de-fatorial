1fa83157ed6a26caf2c98ee8f64249978352bb07
Patrick Daniel <contato.pdani@gmail.com>
2023-03-25 17:09:48 -0300

    remoção de linhas vazias

--- stript.js
+++ stript.js
@@ -1,17 +1,13 @@
 function fatorial(num) {
     let result = num;
     if (num < 0 || Number.isInteger(num) == false) {
-
         return "escreva um numero inteiro positivo";
 
     } else if (num === 0 || num === 1) {
-
         console.log(1)
         return 1;
     } else {
-
         while (num > 1) {
-
             num--;
             result *= num;
         }
