c029606746626fdad77e0c2820d3981e5e14de8f
Patrick Daniel <contato.pdani@gmail.com>
2023-03-25 17:09:03 -0300

    Somente números inteiros e positivos
    Correção de bugs: só serão aceitos números inteiros e positivos

--- stript.js
+++ stript.js
@@ -1,15 +1,20 @@
 function fatorial(num) {
     let result = num;
-    if (num === 0 || num === 1) {
+    if (num < 0 || Number.isInteger(num) == false) {
 
+        return "escreva um numero inteiro positivo";
+
+    } else if (num === 0 || num === 1) {
+
+        console.log(1)
         return 1;
     } else {
+
         while (num > 1) {
 
             num--;
             result *= num;
         }
-
         return result;
     }
 }
