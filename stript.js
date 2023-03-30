2e7dc19abd78a8e59cc8fa737715cd24dc4337e3
Patrick Daniel <contato.pdani@gmail.com>
2023-03-30 11:26:07 -0300

    remoção de console.log

--- stript.js
+++ stript.js
@@ -2,9 +2,7 @@ function fatorial(num) {
     let result = num;
     if (num < 0 || Number.isInteger(num) == false) {
         return "escreva um numero inteiro positivo";
-
     } else if (num === 0 || num === 1) {
-        console.log(1)
         return 1;
     } else {
         while (num > 1) {
